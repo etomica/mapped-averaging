bbb8a05f26efae1b2f7ff5f59877cb4ae4c9e38d
Sabry Moustafa <sabrygad@buffalo.edu>
2018-11-28 11:41:20 -0500

    new

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
+    'sphinx.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.

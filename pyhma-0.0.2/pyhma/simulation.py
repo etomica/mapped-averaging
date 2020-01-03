3f201c7cc72913ca4eca8ffc5d46039ca35aed94
sabrygad <sabrygad@buffalo.edu>
2020-01-03 18:28:44 -0500

    User-defined steps

--- pyhma-0.0.2/pyhma/simulation.py
+++ pyhma-0.0.2/pyhma/simulation.py
@@ -18,64 +18,68 @@ B
 import numpy as np
 import pyhma
 
-
 class Simulation:
-
   """This Simulation class is a class to compute ensemble averages.
 
   """
-
-  def __init__(self, data, pressure_qh, verbose=False):
+  def __init__(self, data, pressure_qh):
     """Retrieve MD simulation data ('data' dict of the read() method) and user-defined parameters ('params' dict).
     """
-    self.temperature   = data['temperature']                       # set NVT emperature (K)
-    self.timestep      = data['timestep']                          # MD timestep size (fs)
-    self.num_atoms     = data['num_atoms']                         # number of atoms
-    self.volume_atom   = data['volume']/self.num_atoms             # specific volume
-    self.box_row_vecs     = np.array(data['box_row_vecs'])               # lattice (raw) vectors (A)
-    self.basis         = np.array(data['basis'])                   # atomic positions of first (minimized) configuration (A)
-    self.positions     = np.array(data['positions'])               # positions at each atom at each MD step ([A , eV/A])
-    self.forces        = np.array(data['forces'])                  # forces at each atom at each MD step ([A , eV/A])
-    self.energies      = np.array(data['energies'])/self.num_atoms # energy of each configuration (eV/atom)
-    self.pressures_vir = data['pressures_vir']                     # virial pressure (without IG) (GPa)
-    self.pressure_qh   = pressure_qh                               # quasiharmonic pressure (GPa)
-    self.steps_tot  = len(self.energies)                               # number of MD steps
-    self.verbose       = verbose
-    self.out_data = np.empty((0,4))  # anharmonic data array ([e_ah_conv, e_ah_hma, p_ah_conv, p_ah_hma])
+    self.temperature   = data['temperature']            # set emperature (K)
+    self.timestep      = data['timestep']               # MD timestep size (fs)
+    self.num_atoms     = data['num_atoms']              # total number of atoms
+    self.volume_atom   = data['volume_atom']            # specific volume (A^3/atom)
+    self.box_row_vecs  = np.array(data['box_row_vecs']) # box edge (raw) vectors (A)
+    self.basis         = np.array(data['basis'])        # atomic positions of initial configuration (fractional)
+    self.positions     = np.array(data['positions'])    # positions at each atom at each MD step (fractional)
+    self.forces        = np.array(data['forces'])       # forces at each atom at each MD step (eV/A)
+    self.energies      = np.array(data['energies'])     # energy of each configuration (eV/atom)
+    self.pressures_vir = data['pressures_vir']          # virial pressure (i.e., without ideal gas) (GPa)
+    self.pressure_qh   = pressure_qh                    # quasiharmonic pressure HMA parameter (GPa)
+    self.out_data      = np.empty((0,4))                # anharmonic data array ([e_ah_conv, e_ah_hma, p_ah_conv, p_ah_hma])
 
-  def run(self):
+  """ compute instantinious 
+  """
+  def run(self, steps_tot, verbose=False):
     """compute Conv and HMA anharmonic energy and pressure.
     """
-    kB = 0.0000861733063733830     # Boltzmann's constant (eV/K)
-    eV2J = 1.60217733e-19          # eV to Joules conversion factor
-    kBT_eV = kB*self.temperature   # eV
-    kBT_J  = kBT_eV*eV2J           # J
-    self.density = 1/self.volume_atom # atom/A^3
-    self.pressure_ig = self.density*1e30*kBT_J*1e-9  # ideal gas pressure (GPa)
-    f_v = (self.pressure_qh*1e9/kBT_J-self.density*1e30)/(3*(self.num_atoms-1))*1e-9 # f_v variable HMA pressure (GPa/J)
+    self.steps_tot  = steps_tot
+    if steps_tot > len(self.energies):
+      print(' WARNING! User-set steps_tot (', steps_tot,') can not be larger than MD simulation steps (', len(self.energies),').')
+      print('          Reduce steps_tot and try again.')
+      return
+
+    kB = 0.0000861733063733830                            # Boltzmann's constant (eV/K)
+    eV2J = 1.60217733e-19                                 # eV to Joules conversion factor
+    kBT_eV = kB*self.temperature                          # eV
+    kBT_J  = kBT_eV*eV2J                                  # J
+    self.density = 1/self.volume_atom                     # atom/A^3
+    self.pressure_ig = self.density*1e30*kBT_J*1e-9       # ideal gas pressure (GPa)
+    f_v = (self.pressure_qh*1e9/kBT_J-self.density*1e30)\
+         /(3*(self.num_atoms-1))*1e-9                     # f_v variable HMA pressure (GPa/J)
     energy_lat   = self.energies[0]
     pressure_lat = self.pressures_vir[0]
-    if self.verbose:
-      print('Computing instantaneous energy and pressure')
-      print('===========================================')
-      print('  simulation data')
-      print('  Temperature           (K): %10.5f' % self.temperature)
-      print('  Volume         (A^3/atom): %10.5f' % self.volume_atom)
-      print('  Timestep             (fs): %10.5f' % self.timestep)
-      print('  Lattice energy  (eV/atom): %10.5f' % energy_lat)
-      print('  Harmonic energy (eV/atom): %10.5f' % (1.5*kBT_eV*(self.num_atoms-1)/self.num_atoms))
-      print('  Lattice pressure    (GPa): %10.5f' % pressure_lat)
-      print('  Harmonic pressure   (GPa): %10.5f' % self.pressure_qh)
-      print('\n  Run ...')
-    
+    if verbose:
+      print('\nSimulation data')
+      print('===============')
+      print(' Set temperature       (K): %10.5f' % self.temperature)
+      print(' Volume         (A^3/atom): %10.5f' % self.volume_atom)
+      print(' MD timestep          (fs): %10.5f' % self.timestep)
+      print(' Lattice energy  (eV/atom): %10.5f' % energy_lat)
+      print(' Harmonic energy (eV/atom): %10.5f' % (1.5*kBT_eV*(self.num_atoms-1)/self.num_atoms))
+      print(' Lattice pressure    (GPa): %10.5f' % pressure_lat)
+      print(' Harmonic pressure   (GPa): %10.5f' % self.pressure_qh)
+      print('\n Found', len(self.energies) ,' total MD steps')
+      print(' Using ', self.steps_tot, ' user-set MD steps')
+      print('\n Computing instantaneous properties ...')
+ 
     basis_cart = Simulation._direct_to_cart(self.basis, self.box_row_vecs)
-    with open('e_ah.out','w') as file_energies_ah, open('p_ah.out','w') as file_pressures_ah:
+    with open('energy_ah.out','w') as file_energy_ah, open('pressure_ah.out','w') as file_pressure_ah:
       self._nearest_image = pyhma.NearestImage(self.box_row_vecs) 
-      self.steps_tot  = len(self.energies)  # number of MD steps
       for step in range(self.steps_tot): # snaps
         sim_time = step*self.timestep
         # Conv
-        e_ah_conv = self.energies[step]-energy_lat - 1.5*kBT_eV*(self.num_atoms-1)/self.num_atoms
+        e_ah_conv = self.energies[step] - energy_lat - 1.5*kBT_eV*(self.num_atoms-1)/self.num_atoms
         p_ah_conv = self.pressures_vir[step] + self.pressure_ig - pressure_lat - self.pressure_qh
         # HMA
         r_cart  = Simulation._direct_to_cart(self.positions[step], self.box_row_vecs)
@@ -89,30 +93,34 @@ class Simulation:
           self._nearest_image.get_nearest_image(dr)
           fdr = fdr + f.dot(dr)
 
-        e_ah_hma  = self.energies[step]-energy_lat + 0.5*fdr/self.num_atoms
+        e_ah_hma  = self.energies[step] + 0.5*fdr/self.num_atoms - energy_lat 
         p_ah_hma  = self.pressures_vir[step] + f_v*fdr*eV2J - pressure_lat
-        print('%10.1f  %10.5f  %10.5f' % (sim_time, e_ah_conv, e_ah_hma) , file=file_energies_ah)
-        print('%10.1f  %10.5f  %10.5f' % (sim_time, p_ah_conv, p_ah_hma) , file=file_pressures_ah)
+        print('%10.1f  %10.5f  %10.5f' % (sim_time, e_ah_conv, e_ah_hma) , file=file_energy_ah)
+        print('%10.1f  %10.5f  %10.5f' % (sim_time, p_ah_conv, p_ah_hma) , file=file_pressure_ah)
         self.out_data = np.append(self.out_data , [[e_ah_conv, e_ah_hma, p_ah_conv, p_ah_hma]] , axis=0)
 
-    if self.verbose:
-      print('  Done computing instantaneous Conv and HMA anharmonic energy and pressure for', self.steps_tot, 'MD steps.\n')
-
-
-
-  # compute statistics: averages, uncertainties, and autocorrelations
-  def get_stats(self, steps_eq, blocksize):
+  # Compute statistics: average (avg), stochastic uncertainty (err), and correlation (cor)
+  def get_stats(self, steps_eq, blocksize, verbose=False):
     """get_stats() function!!!
     """
+    if self.steps_tot < steps_eq:
+      print('WARNING! Number of equilibaration steps (', steps_eq,') can not be larger than total steps (', self.steps_tot,').')
+      print('         Reduce steps_eq and try again.')
+      return
+    elif int((self.steps_tot - steps_eq)/blocksize) == 0:
+      print('WARNING! Number of blocks ((steps_tot-steps_eq)/blocksize) is zero.')
+      print('         Reduce blocksize to get finite number of blocks and try again.')
+      return 
 
     n_prod = self.steps_tot - steps_eq
-    data_prod = self.out_data[steps_eq:,:]
+    data_prod = self.out_data[steps_eq:self.steps_tot,:]
 
-    if self.verbose:
-      print('Block averaging statistics')
+    if verbose:
+      print('\nBlock averaging statistics')
       print('==========================')
-      print(' ', n_prod, 'production steps (after', steps_eq ,'equilibration steps)')
-      print(' ',int((self.steps_tot-steps_eq)/blocksize), 'blocks (blocksize =' ,  blocksize,' steps)\n')
+      print('', n_prod, 'production steps (after', steps_eq ,'equilibration steps)')
+      print('',int((self.steps_tot-steps_eq)/blocksize), 'blocks (blocksize =' ,  blocksize,' steps)\n')
+      print(' Computing statistics ...')
 
     data_prod_block = Simulation._block_data(data_prod, blocksize) 
     # Averages
@@ -120,29 +128,33 @@ class Simulation:
     # Uncertainty
     data_err = np.std(data_prod_block, ddof = 1, axis=0)/np.sqrt(len(data_prod_block))
     # Correlation
-    data_ac = Simulation._get_ac(data_prod_block)
+    data_cor = Simulation._get_cor(data_prod_block)
 
-    self.out_stats = {'e_ah_conv': {'avg': data_avg[0] , 'err': data_err[0] , 'cor': data_ac[0]},\
-                      'e_ah_hma' : {'avg': data_avg[1] , 'err': data_err[1] , 'cor': data_ac[1]}, \
-                      'p_ah_conv': {'avg': data_avg[2] , 'err': data_err[2] , 'cor': data_ac[2]}, \
-                      'p_ah_hma' : {'avg': data_avg[3] , 'err': data_err[3] , 'cor': data_ac[3]}}
+    self.out_stats = {'e_ah_conv': {'avg': data_avg[0] , 'err': data_err[0] , 'cor': data_cor[0]},\
+                      'e_ah_hma' : {'avg': data_avg[1] , 'err': data_err[1] , 'cor': data_cor[1]}, \
+                      'p_ah_conv': {'avg': data_avg[2] , 'err': data_err[2] , 'cor': data_cor[2]}, \
+                      'p_ah_hma' : {'avg': data_avg[3] , 'err': data_err[3] , 'cor': data_cor[3]}}
     return self.out_stats
 
 
+  """Print statistics (avg, err, cor)
+  """
   @staticmethod
   def print_stats(out_stats):
     """ PRINT out_stats
     """
-    print('  e_ah_conv (meV/atom): %10.5f +/- %5.1e    cor: %3.2f' % (1e3*out_stats['e_ah_conv']['avg'],\
+    print('\n e_ah_conv (meV/atom): %10.5f +/- %5.1e    cor: %3.2f' % (1e3*out_stats['e_ah_conv']['avg'],\
             1e3*out_stats['e_ah_conv']['err'], out_stats['e_ah_conv']['cor']))
-    print('  e_ah_hma  (meV/atom): %10.5f +/- %5.1e    cor: %3.2f' % (1e3*out_stats['e_ah_hma']['avg'],\
+    print(' e_ah_hma  (meV/atom): %10.5f +/- %5.1e    cor: %3.2f' % (1e3*out_stats['e_ah_hma']['avg'],\
             1e3*out_stats['e_ah_hma']['err'], out_stats['e_ah_hma']['cor']))
-    print('  p_ah_conv      (GPa): %10.5f +/- %5.1e    cor: %3.2f' % (out_stats['p_ah_conv']['avg'],\
+    print(' p_ah_conv      (GPa): %10.5f +/- %5.1e    cor: %3.2f' % (out_stats['p_ah_conv']['avg'],\
              out_stats['p_ah_conv']['err'], out_stats['p_ah_conv']['cor']))
-    print('  p_ah_hma       (GPa): %10.5f +/- %5.1e    cor: %3.2f\n' % (out_stats['p_ah_hma']['avg'],\
+    print(' p_ah_hma       (GPa): %10.5f +/- %5.1e    cor: %3.2f\n' % (out_stats['p_ah_hma']['avg'],\
              out_stats['p_ah_hma']['err'], out_stats['p_ah_hma']['cor']),end='')
 
 
+  """Block data to blocks of size blocksize
+  """
   @staticmethod
   def _block_data(data, blocksize):
     """ block data!!!
@@ -161,8 +173,11 @@ class Simulation:
         n+=1
     return data_block
 
+
+  """Compute autocorrelation between adjacent data points
+  """
   @staticmethod
-  def _get_ac(data):  # without the N-1 correction in both numerators and denominators
+  def _get_cor(data):  # without the N-1 correction in both numerators and denominators
     """ AC of data!!!
     """
     data_avg = np.average(data, axis=0)
@@ -171,9 +186,12 @@ class Simulation:
     for i in range(len(data)-1): # 0,1,... n-2
       sum += (data[i]-data_avg)*(data[i+1]-data_avg)
     sum *= 1/(len(data)-1)
-    ac = sum/data_var
-    return ac
+    cor = sum/data_var
+    return cor
 
+
+  """Convert direct (fractional) to Cartesian coordinates, for a given box vectors a.
+  """
   @staticmethod
   def _direct_to_cart(x, a):
     r = np.empty((0,3))
@@ -182,12 +200,3 @@ class Simulation:
       r = np.append(r, [r_i], axis=0)
     return r  
  
-
-
-
-
-
-
-
-
-
--- pyhma-0.0.2/pyhma/vasprun_reader.py
+++ pyhma-0.0.2/pyhma/vasprun_reader.py
@@ -3,7 +3,7 @@ import sys, os
 import lxml.etree 
 
 def read(vasprun_files, force_tol=0.001, raw_files=False, verbose=False):
-  box_row_vecs  = [] # box edge (row) vectors  
+  box_row_vecs   = [] # box edge (row) vectors  
   basis          = [] 
   positions      = [] 
   forces         = [] 
@@ -11,17 +11,16 @@ def read(vasprun_files, force_tol=0.001, raw_files=False, verbose=False):
   pressures_vir  = [] 
   n_files        = len(vasprun_files)
   parser         = lxml.etree.XMLParser(recover=True) 
+  list_len       = 0  
 
   for i, vasprun_file_i in enumerate(vasprun_files): 
     tree = lxml.etree.parse(vasprun_file_i, parser) 
     if verbose: 
       if i == 0: 
-        print('Reading' , *vasprun_files, 'files') 
+        print('\nReading' , *vasprun_files) 
         print('==============================================') 
-        print('  first configuration data from', vasprun_files[0]) 
-        print('  ----------------------------------------------') 
-      else: 
-        print('  Reading' , vasprun_file_i ,'data (', i+1,'out of' , n_files, ') ... ')
+        print(' first configuration data from', vasprun_files[0]) 
+        print(' ----------------------------------------------') 
 
     # Extract simulation parameters
     if i == n_files-1: 
@@ -30,8 +29,8 @@ def read(vasprun_files, force_tol=0.001, raw_files=False, verbose=False):
 
     # Extract first step information
     if i == 0: 
-      num_atoms = int(tree.find("./atominfo/atoms").text) # number of atoms
-      volume  = float(tree.find("./structure/crystal/i[@name='volume']").text) # volume
+      num_atoms   = int(tree.find("./atominfo/atoms").text) # number of atoms
+      volume_atom = float(tree.find("./structure/crystal/i[@name='volume']").text)/num_atoms # specific volume
       # box edge (row) vectors
       for v in tree.find("./structure/crystal/varray[@name='basis']"): 
         box_row_vecs.append([float(x) for x in v.text.split()]) 
@@ -42,12 +41,26 @@ def read(vasprun_files, force_tol=0.001, raw_files=False, verbose=False):
       # extract initial forces (must be smaller than the user-defined force tolerance, force_tol)
       forces_0 = [] 
       for v in tree.find("./calculation/varray[@name='forces']"): 
-        forces_0.append([float(x) for x in v.text.split()]) 
+        forces_0.append([float(x) for x in v.text.split()])
+      # print initial configuration structure and forces 
+      if verbose:
+        print('',num_atoms, 'atoms (total)')
+        print(' Box edge (row) vectors')
+        for l in range(len(box_row_vecs)):     
+          print('%12.8f %12.8f %12.8f ' % (*box_row_vecs[l],))
+        print('\n atom       xyz (direct) coordinates (A)                   xyz forces (eV/A)')
+        for l in range(len(forces_0)):
+          print(' %3d  %12.8f %12.8f %12.8f    %12.8f %12.8f %12.8f' % (l+1, *basis[l], *forces_0[l]))
+        print('')
+
       # check for equilubrium
       if _is_large_force(forces_0, force_tol) == True:
         print(' MUST START FROM MINIMIZED CONFIGURATION (ZERO FORCES).')
         print(' EXITING pyHMA!\n')
         return
+
+    if verbose:
+      print('  Reading' , vasprun_file_i ,' (', i+1,'out of' , n_files, ')')
 
     # Extract positions
     for pos_elem in tree.findall("./calculation/structure/varray[@name='positions']"): 
@@ -65,7 +78,7 @@ def read(vasprun_files, force_tol=0.001, raw_files=False, verbose=False):
     
     # Extract energies (E0)
     for e in tree.findall("./calculation/energy/i[@name='e_wo_entrp']"): 
-      energies.append(float(e.text)) 
+      energies.append(float(e.text)/num_atoms) 
  
      
     # Extract virial pressures (i.e., total pressure - ideal gas)
@@ -78,16 +91,15 @@ def read(vasprun_files, force_tol=0.001, raw_files=False, verbose=False):
       pressures_vir.append(pvir) 
     
     # resize the length of all data lists
-    min_len =  len(tree.findall("./calculation/energy/i[@name='total']"))
-    positions     = positions[0:min_len]
-    forces        = forces[0:min_len]
-    energies      = energies[0:min_len]
-    pressures_vir = pressures_vir[0:min_len]
-
+    list_len +=  len(tree.findall("./calculation/energy/i[@name='total']"))
+    positions     = positions[0:list_len]
+    forces        = forces[0:list_len]
+    energies      = energies[0:list_len]
+    pressures_vir = pressures_vir[0:list_len]
   if raw_files: 
     _make_raw_files(num_atoms, box_row_vecs, basis, positions, forces, energies, pressures_vir)
  
-  return {'box_row_vecs': box_row_vecs, 'num_atoms': num_atoms, 'volume': volume, 'basis': basis, 'positions': positions,\
+  return {'box_row_vecs': box_row_vecs, 'num_atoms': num_atoms, 'volume_atom': volume_atom, 'basis': basis, 'positions': positions,\
           'forces': forces, 'energies': energies, 'pressures_vir': pressures_vir, 'timestep': timestep, 'temperature': temperature}
 
 
@@ -96,32 +108,42 @@ def _is_large_force(f, tol):
   for i,v in enumerate(f):
     v_magn = np.linalg.norm(v)
     if v_magn > tol:
-      print(' WARNING! Magnitude of force on atom ',(i+1),' = ', v_magn, ' (> tol = ', tol,')')
+      print(' WARNING! Magnitude of force on atom ',(i+1),' is ', v_magn, ' (> tol = ', tol,').')
       is_large_force = True
   return is_large_force
 
 
 def  _make_raw_files(num_atoms, box_row_vecs, basis, positions, forces, energies, pressures_vir):
-    file_poscar_eq     = open('poscar_eq.dat', 'w')
-    file_posfor        = open('posfor.dat', 'w')
-    file_energies      = open('energies.dat', 'w')
-    file_pressures_vir = open('pressures_vir.dat', 'w')
+    file_poscar_eq    = open('poscar_eq.dat', 'w')
+    file_posfor       = open('posfor.dat', 'w')
+    file_energy       = open('energy.dat', 'w')
+    file_pressure_vir = open('pressure_vir.dat', 'w')
     # generate poscar_eq file 
     print('Lattice vectors\n1.0 scaling factor', file = file_poscar_eq)
-    np.savetxt(file_poscar_eq, box_row_vecs, fmt='%12.8f')
-    print(num_atoms,' atoms (total)\nDirect',file=file_poscar_eq)
-    np.savetxt(file_poscar_eq, basis, fmt='%12.8f')
-    # generate posfor file
-    for t in range(len(positions)):
-      print(t, file=file_posfor)
-      np.savetxt(file_posfor, np.concatenate((positions[t] ,forces[t]),axis=1), fmt='%12.8f')
-    # generate energies file
-    np.savetxt(file_energies, energies, fmt='%12.8f')
-    # generate virial pressure file
-    np.savetxt(file_pressures_vir, pressures_vir, fmt='%12.8f')
+    for i in range(len(box_row_vecs)):
+      print('%12.8f %12.8f %12.8f' % (*box_row_vecs[i],) , file=file_poscar_eq)
+    print(num_atoms,' atoms (total)\nDirect', file=file_poscar_eq)
+    for i in range(len(basis)):
+      print('%12.8f %12.8f %12.8f' % (*basis[i],) , file=file_poscar_eq)
  
+
+    # generate posfor file
+    for i in range(len(positions)):
+      print(i, file=file_posfor)
+      for j in range(len(positions[i])):  
+        print('%12.8f %12.8f %12.8f    %12.8f %12.8f %12.8f' % (*positions[i][j], *forces[i][j]), file=file_posfor)
+
+    # generate energies file
+    for i in range(len(energies)):
+      print('%12.8f' % (energies[i]) , file=file_energy)
+ 
+    # generate virial pressure file
+    for i in range(len(pressures_vir)):
+      print('%12.8f' % (pressures_vir[i]) , file=file_pressure_vir)
+ 
+    # close all files 
     file_poscar_eq.close()
     file_posfor.close()
-    file_energies.close()
-    file_pressures_vir.close()
+    file_energy.close()
+    file_pressure_vir.close()
  

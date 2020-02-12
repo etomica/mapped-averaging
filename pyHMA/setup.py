b56afdd90bfb0bdbc423e8cf174b1f0109085800
sabrygad <sabrygad@buffalo.edu>
2020-02-12 12:07:37 -0500

    changed pyhma to pyHMA

--- pyHMA/setup.py
+++ pyHMA/setup.py
@@ -5,11 +5,11 @@ setup.py
 from setuptools import setup, Extension
 
 
-setup(name='pyhma',
-      version='0.0.2',
+setup(name='pyHMA',
+      version='1.0.0',
       description='Python Distribution Utilities',
       author='Sabry Moustafa',
       author_email='sabrygad@buffalo.edu',
-      packages=['pyhma'],
-      scripts=['scripts/pyhma']
+      packages=['pyHMA'],
+      scripts=['scripts/pyHMA']
       )

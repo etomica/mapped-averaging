927824c53334cc07524094ed1538e26e59eb35cd
sabrygad <sabrygad@buffalo.edu>
2020-09-30 14:12:28 -0400

    Included Fermi-Dirac and added VASP details

--- pyhma/setup.py
+++ pyhma/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, Extension
 
 
 setup(name='pyhma',
-      version='2.0.0',
+      version='2.0.1',
       description='Python Distribution Utilities',
       author='Sabry Moustafa',
       author_email='sabrygad@buffalo.edu',

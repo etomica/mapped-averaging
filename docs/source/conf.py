3e8a85064684c7ac51d21548b6826000fa89d109
sabrygad <sabrygad@buffalo.edu>
2020-10-05 14:16:04 -0400

    Included Fermi-Dirac and added VASP details

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,9 @@ copyright = '2018, Sabry Moustafa'
 author = 'Sabry Moustafa'
 
 # The short X.Y version
-version = '0.0.1'
+version = '2.0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------

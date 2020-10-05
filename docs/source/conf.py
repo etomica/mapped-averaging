87e449aecd91ad1a57b0ce1ffeb5692a97f615eb
sabrygad <sabrygad@buffalo.edu>
2020-10-05 16:29:25 -0400

    pyhma

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
+#import os
+#import sys
+#sys.path.insert(0, os.path.abspath('../../pyhma/pyhma'))
 
 # -- Project information -----------------------------------------------------
 

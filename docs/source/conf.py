475455457c1e89362902f5bbe2512ee06a37007b
sabrygad <sabrygad@buffalo.edu>
2020-10-05 15:27:47 -0400

    Added pyhma to mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ extensions = [
     'sphinx.ext.napoleon'
 ]
 
-autodoc_mock_imports = ['numpy']
+autodoc_mock_imports = ['numpy','pyhma']
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None), 'numpy': ('https://numpy.org/doc/stable', None)}
 
 
